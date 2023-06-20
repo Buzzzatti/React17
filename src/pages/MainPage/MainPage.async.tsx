2d6dacccf89edf2dd9058fe1fa1be81c41a88dbc
Buzaev Roman <rbuzaev1@gmail.com>
2023-06-20 22:35:13 +0400

    Lesson 6 React routing React routing dom Build bundle decomposition Suspense routing/lazy loading

---
+++ src/pages/MainPage/MainPage.async.tsx
@@ -0,0 +1,8 @@
+import { resolve } from 'path';
+import {lazy} from 'react'
+
+export const MainPageAsync = lazy(() => new Promise((resolve)=>{
+    //@ts-ignore
+    //ТАК НЕ СТОИТ ДЕЛАТЬ В ЖИЗНИ
+    setTimeout(()=> resolve(import('./MainPage')), 1500)
+}));
