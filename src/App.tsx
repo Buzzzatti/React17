cff7184a8fee86a1ade60ec5531cc13075ed4f62
Buzaev Roman <rbuzaev1@gmail.com>
2023-06-20 20:32:23 +0400

    Lesson 5 Added mini css to create css files for build Added css.modules for styles isolation

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Counter } from "./components/Counter";
+import './index.scss'
+
+const App = () => {
+    return (
+        <div className="app">
+            Check font size
+            <Counter/>
+        </div>
+    )
+}
+export default App;
