28d5b4a334d1ce8cae58d5220377acc5c6314570
Buzaev Roman <rbuzaev1@gmail.com>
2023-06-22 14:27:34 +0400

    Lesson 11 AppRouter config

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
-import { Link, Route, Routes } from "react-router-dom";
+import { Link} from "react-router-dom";
 import './styles/index.scss'
-import { Suspense } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
-import { AboutPage } from "pages/AboutPage";
-import { MainPage } from "pages/MainPage";
+import { AppRouter } from "./providers/router";
 
 
 
@@ -16,12 +14,7 @@ const App = () => {
             <button onClick={toggleTheme}>Toggle</button>
             <Link to={'/'} >Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />}/>
-                    <Route path={'/'} element={<MainPage />}/>
-                </Routes>
-            </Suspense>
+            <AppRouter/>
         </div>
     )
 }
